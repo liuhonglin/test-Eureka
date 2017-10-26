bf6c3bf2de5e77e695decc6d465c3607e58f5447
liuhonglin <liuhonglinh@163.com>
2017-10-27 00:04:28 +0800

    first commit server terminal code.

---
+++ eureka-server/src/main/java/lhl/test/springcloud/eurekaServer/Application.java
@@ -0,0 +1,17 @@
+package lhl.test.springcloud.eurekaServer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by HL.L on 2017/10/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
